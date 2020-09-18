1511c8a80194b42f58bc7304ff6cf2998251763a
dngatimin <dngatimin95@gmail.com>
2020-09-18 10:55:11 -0400

    Fluff stuff

--- feels-analysis.py
+++ feels-analysis.py
@@ -2,6 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import scikitplot as skplt
 import speech_recognition as sr
+import time
+from playsound import playsound
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
@@ -34,7 +36,7 @@ def check_mic(recognizer, mic):
 
 def ask_user():
     while True:
-        ans = input("\nWelcome! Do you want to talk or type to me? (Ta = Talk, Ty = Type, Ex = Exit)\n")
+        ans = input("\nHi there! Do you want to talk or type to me? (Ta = Talk, Ty = Type, Ex = Exit)\n")
         ans = ans.strip().lower()
         if ans == "ta":
             log_accuracy, nb_accuracy = train()
@@ -85,22 +87,21 @@ def train():
     return log_accuracy, nb_accuracy
 
 def types(log_accuracy, nb_accuracy):
-    user_input = input("\nHow are you feeling today?\n")
+    user_input = input("\nHow are you feeling today? Tell me all about it.\n")
     ans = vect.transform([user_input])
     feels = sentiment(log_accuracy, nb_accuracy, ans)
 
     if feels == "Negative":
-        print("I'm sorry about your day. Why dont you take a rest?")
+        user_type()
     else:
         print("That's great! I hope you keep up this energy!")
     exit()
 
-def talk(log_accuracy, nb_accuracy):
+def answer():
     tries = 5
     recognizer = sr.Recognizer()
     mic = sr.Microphone()
     for i in range(tries):
-        print('Tell me how you feel today.')
         guess = check_mic(recognizer, mic)
         if guess["transcription"]:
             break
@@ -113,12 +114,17 @@ def talk(log_accuracy, nb_accuracy):
         exit()
 
     print("You said: {}".format(guess["transcription"]))
+    return guess["transcription"]
 
-    ans = vect.transform([guess["transcription"]])
+def talk(log_accuracy, nb_accuracy):
+    print('Tell me how you feel today.')
+    recorded_ans = answer()
+
+    ans = vect.transform([recorded_ans])
     feels = sentiment(log_accuracy, nb_accuracy, ans)
 
     if feels == "Negative":
-        print("I'm sorry about your day. Why dont you take a rest?")
+        user_talk()
     else:
         print("That's great! I hope you keep up this energy!")
     exit()
@@ -140,5 +146,75 @@ def sentiment(log_accuracy, nb_accuracy, ans):
     print("Sentiment: " + str(prediction[0]))
 
     return prediction[0]
+
+def user_type():
+    while True:
+        reply = input("I'm sorry about your day. Why dont we do a 90 second relaxation exercise to calm down? (Y/N)\n")
+        if reply.strip().lower() == "y":
+            print("\nIn order to get the most out of this experience, please put away any distractions and turn on the volume.")
+            time.sleep(0.2)
+            while True:
+                start = input("Squeeze enter to start\n")
+                if start == "":
+                    negative_feels()
+                else:
+                    trys = input("I'm sorry do you want to try again? (Y/N)\n")
+                    if trys.strip().lower() == "y":
+                        continue
+                    elif trys.strip().lower() == "n":
+                        print("Alright then. I hope you feel better soon. Goodbye")
+                        exit()
+                    else: print("I'm really sorry but please try again")
+        elif reply.strip().lower() == "n":
+            print("Alright then. I hope you feel better soon. Goodbye")
+            exit()
+        else:
+            print("I'm really sorry but please try again")
+
+def user_talk():
+    while True:
+        print("I'm sorry about your day. Why dont we do a 90 second relaxation exercise to calm down? (Yes or No)")
+        yn_ans = answer()
+        if yn_ans == "yes":
+            print("\nIn order to get the most out of this experience, please put away any distractions and turn on the volume.")
+            time.sleep(1)
+            while True:
+                print("Are you ready? (Yes or No)")
+                ready = answer()
+                if ready == "yes":
+                    negative_feels()
+                else:
+                    print("I'm sorry do you want to try again? (Yes or No)")
+                    ready = answer()
+                    if ready == "y":
+                        continue
+                    elif ready == "n":
+                        print("Alright then. I hope you feel better soon. Goodbye")
+                        exit()
+                    else: print("I'm really sorry but please try again")
+        elif yn_ans == "no":
+            print("Alright then. I hope you feel better soon. Goodbye")
+            exit()
+        else:
+            print("I'm really sorry but please try again")
+
+def negative_feels():
+    kalm_words = ["relax...", "...think happy thoughts...", "...everything is ok...", "...you have 0 notifications to read...",
+    "...emails can wait...", "...calm your mind...", "...stretch...", "...there is nothing that cannot wait...", "...smile :)",
+    "...take a big, deep breathe...", "...now exhale...", "...feel all the tension dissipate...", "...rest your eyes for 10 seconds..."]
+    playsound('C:\\Users\\Darren\\Documents\\Github\\ML-Deep-Dive\\Quiet_Time.mp3', False)
+    for i in kalm_words:
+        print(i + "\n")
+        time.sleep(5)
+    time.sleep(10)
+    print("I hope you feel better now. Take this time to relax some more.")
+    time.sleep(20)
+    while True:
+        leave = input("Ready to leave? Squeeze enter again")
+        if leave == "":
+            print("Goodbye!")
+            exit()
+        else:
+            continue
 
 ask_user()
